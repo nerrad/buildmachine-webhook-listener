9b1f37c0f2ac416e5a18a7b67fd93a3fc1ef5073
Darren Ethier <darren@roughsmootheng.in>
2015-01-10 01:54:56 -0500

    adding sleep

--- inc/React.php
+++ inc/React.php
@@ -63,7 +63,8 @@ class React {
 				 exec( 'cd ~/buildmachine/event-espresso-core && grunt bumprc_' . $ref . ' 2>&1', $output );
 				 //let's output to syslog
 				 syslog( LOG_DEBUG, print_r( $output, true ) );
-				 exec( 'cd !/buildmachine/event-espresso-core && grunt updateSandbox_' . $ref . '2>&1', $$output2 );
+				 sleep(3); //give some time for the above to finish!
+				 exec( 'cd ~/buildmachine/event-espresso-core && grunt updateSandbox_' . $ref . '2>&1', $$output2 );
 				 syslog( LOG_DEBUG, print_r( $output2 ) );
 				break;
 
